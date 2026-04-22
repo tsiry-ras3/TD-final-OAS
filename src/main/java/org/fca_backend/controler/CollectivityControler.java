982c7183ac8b867dbd5edfaf8ba8345e7f94adf5
Tsiry <hei.tsiry@gmail.com>
2026-04-23 00:00:43 +0300

    feat: add controller for transaction

--- src/main/java/org/fca_backend/controler/CollectivityControler.java
+++ src/main/java/org/fca_backend/controler/CollectivityControler.java
@@ -5,6 +5,7 @@ import org.fca_backend.DTO.CreateCollectivityDTO;
 import org.fca_backend.DTO.UpdateCollectivityDTO;
 import org.fca_backend.entity.Collectivity;
 import org.fca_backend.exception.BadRequestException;
+import org.fca_backend.repository.CollectivityTransactionRepository;
 import org.fca_backend.service.CollectivityService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 public class CollectivityControler {
     CollectivityService collectivityService;
+    CollectivityTransactionRepository ctRepository;
 
     @PostMapping("/collectivities")
     public ResponseEntity<?> createCollectivities(@RequestBody List<CreateCollectivityDTO> createCollectivityDTO){
@@ -35,6 +37,14 @@ public class CollectivityControler {
             return ResponseEntity.status(HttpStatus.OK).body(collectivity);
         } catch (BadRequestException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+    @GetMapping("/collectivites/{id}/transactions")
+    public ResponseEntity<?> getTransactions(@PathVariable String id){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(ctRepository.getCollectivityTransaction(id));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
