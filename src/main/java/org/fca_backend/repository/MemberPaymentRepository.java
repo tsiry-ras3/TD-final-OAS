762d9782a14fd09223c15a283a9282a75a918e43
Tsiry <hei.tsiry@gmail.com>
2026-04-22 21:46:15 +0300

    fix: change switch case to if else

--- src/main/java/org/fca_backend/repository/MemberPaymentRepository.java
+++ src/main/java/org/fca_backend/repository/MemberPaymentRepository.java
@@ -116,37 +116,34 @@ public class MemberPaymentRepository {
     private FinancialAccount mapFinancialAccount(ResultSet rs) throws SQLException {
         String type = rs.getString("account_type");
 
-        return switch (type) {
-            case "CASH" -> {
-                CashAccount account = new CashAccount();
-                account.setId(rs.getString("id"));
-                account.setAmount(rs.getDouble("amount"));
-                yield account;
-            }
-            case "MOBILE_BANKING" -> {
-                MobileBankingAccount account = new MobileBankingAccount();
-                account.setId(rs.getString("id"));
-                account.setAmount(rs.getDouble("amount"));
-                account.setHolderName(rs.getString("holder_name"));
-                account.setMobileBankingService(
-                        MobileBankingService.valueOf(rs.getString("mobile_banking_service"))
-                );
-                account.setMobileNumber(rs.getString("mobile_number"));
-                yield account;
-            }
-            case "BANK" -> {
-                BankAccount account = new BankAccount();
-                account.setId(rs.getString("id"));
-                account.setAmount(rs.getDouble("amount"));
-                account.setHolderName(rs.getString("holder_name"));
-                account.setBankName(Bank.valueOf(rs.getString("bank_name")));
-                account.setBankCode(rs.getInt("bank_code"));
-                account.setBankBranchCode(rs.getInt("bank_branch_code"));
-                account.setBankAccountNumber(rs.getInt("bank_account_number"));
-                account.setBankAccountKey(rs.getInt("bank_account_key"));
-                yield account;
-            }
-            default -> throw new SQLException("Unknown account type: " + type);
-        };
+        if ("CASH".equals(type)) {
+            CashAccount account = new CashAccount();
+            account.setId(rs.getString("id"));
+            account.setAmount(rs.getDouble("amount"));
+            return account;
+        } else if ("MOBILE_BANKING".equals(type)) {
+            MobileBankingAccount account = new MobileBankingAccount();
+            account.setId(rs.getString("id"));
+            account.setAmount(rs.getDouble("amount"));
+            account.setHolderName(rs.getString("holder_name"));
+            account.setMobileBankingService(
+                    MobileBankingService.valueOf(rs.getString("mobile_banking_service"))
+            );
+            account.setMobileNumber(rs.getString("mobile_number"));
+            return account;
+        } else if ("BANK".equals(type)) {
+            BankAccount account = new BankAccount();
+            account.setId(rs.getString("id"));
+            account.setAmount(rs.getDouble("amount"));
+            account.setHolderName(rs.getString("holder_name"));
+            account.setBankName(Bank.valueOf(rs.getString("bank_name")));
+            account.setBankCode(rs.getInt("bank_code"));
+            account.setBankBranchCode(rs.getInt("bank_branch_code"));
+            account.setBankAccountNumber(rs.getInt("bank_account_number"));
+            account.setBankAccountKey(rs.getInt("bank_account_key"));
+            return account;
+        } else {
+            throw new SQLException("Unknown account type: " + type);
+        }
     }
 }
