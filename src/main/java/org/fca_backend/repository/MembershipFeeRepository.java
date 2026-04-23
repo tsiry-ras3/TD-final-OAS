98b7c7f504441c15c091876bcbc468ccc51af149
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-04-23 10:09:59 +0300

    feat : implement Membershipfees post methods

--- src/main/java/org/fca_backend/repository/MembershipFeeRepository.java
+++ src/main/java/org/fca_backend/repository/MembershipFeeRepository.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.sql.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,31 +17,70 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class MembershipFeeRepository {
-    private DataSourceConfig dataSourceConfig; 
+    private DataSourceConfig dataSourceConfig;
 
-    public List<MembershipFee> findByCollectivityId (UUID id) {
+    public List<MembershipFee> findByCollectivityId(UUID id) {
         String sql = """
-            SELECT id, collectivity_id, label, eligible_from, frequency, amount, status, created_at, updated_at
-            FROM membership_fees
-            WHERE collectivity_id = ?
-            ORDER BY created_at DESC
-        """;
+                    SELECT id, collectivity_id, label, eligible_from, frequency, amount, status, created_at, updated_at
+                    FROM membership_fees
+                    WHERE collectivity_id = ?
+                    ORDER BY created_at DESC
+                """;
 
         List<MembershipFee> fees = new ArrayList<>();
         try (
-            Connection conn = dataSourceConfig.dataSource().getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql)) {
-                stmt.setObject(1, id);
-                try (ResultSet rs = stmt.executeQuery()) {
-                    while (rs.next()) {
-                        fees.add(mapRow(rs));
-                    }
+                Connection conn = dataSourceConfig.dataSource().getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setObject(1, id);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    fees.add(mapRow(rs));
+                }
             }
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
-        } 
+        }
 
         return fees;
+    }
+
+    //
+    public List<MembershipFee> saveAll(UUID collectivityId, List<MembershipFee> fees) {
+        String sql = """
+            INSERT INTO membership_fees 
+            (collectivity_id, eligible_from, frequency, amount, label, status)
+            VALUES (?, ?, ?, ?, ?, ?)
+            RETURNING id, created_at, updated_at
+        """;
+
+        List<MembershipFee> created = new ArrayList<>();
+        try (Connection conn = dataSourceConfig.dataSource().getConnection()) {
+            conn.setAutoCommit(false);
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                for (MembershipFee fee : fees) {
+                    stmt.setObject(1, collectivityId);
+                    stmt.setDate(2, Date.valueOf(fee.getEligibleFrom()));
+                    stmt.setString(3, fee.getFrequency().name());
+                    stmt.setBigDecimal(4, fee.getAmount());
+                    stmt.setString(5, fee.getLabel());
+                    stmt.setString(6, ActivityStatus.ACTIVE.name());
+                    try (ResultSet rs = stmt.executeQuery()) {
+                        if (rs.next()) {
+                            MembershipFee saved = mapRow(rs);
+                            saved.setCollectivityId(collectivityId);
+                            created.add(saved);
+                        }
+                    }
+                }
+                conn.commit();
+            } catch (SQLException e) {
+                conn.rollback();
+                throw e;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Database error: " + e.getMessage(), e);
+        }
+        return created;
     }
 
     private MembershipFee mapRow(ResultSet rs) throws SQLException {
--- src/main/java/org/fca_backend/service/MembershipFeeService.java
+++ src/main/java/org/fca_backend/service/MembershipFeeService.java
@@ -9,14 +9,18 @@ import org.fca_backend.entity.MembershipFee;
 import org.fca_backend.repository.CollectivityRepository;
 import org.fca_backend.repository.MembershipFeeRepository;
 import org.fca_backend.validator.CollectivityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MembershipFeeService {
+    @Autowired
     private MembershipFeeRepository feeRepository;
+    @Autowired
     private CollectivityRepository collectivityRepository;
 
-    public List<MembershipFeeDTO> getFeeByCollectivityId (UUID collectivityId) {
+    // get fees service
+    public List<MembershipFeeDTO> getFeeByCollectivityId(UUID collectivityId) {
         // validator
         if (!collectivityRepository.existsById(collectivityId)) {
             throw new CollectivityNotFoundException(collectivityId);
@@ -28,14 +32,25 @@ public class MembershipFeeService {
                 .collect(Collectors.toList());
     }
 
-        private MembershipFeeDTO toResponse(MembershipFee fee) {
+    // create fees service
+    public List<MembershipFeeDTO> createFees (UUID collectivityId, List<MembershipFee> feesToCreate) {
+        if (!collectivityRepository.existsById(collectivityId)) {
+            throw new CollectivityNotFoundException(collectivityId);
+        }
+
+        List<MembershipFee> createdFees = feeRepository.saveAll(collectivityId, feesToCreate);
+        return createdFees.stream()
+                .map(this::toResponse)
+                .collect(Collectors.toList());
+    }
+
+    private MembershipFeeDTO toResponse(MembershipFee fee) {
         return new MembershipFeeDTO(
                 fee.getId(),
                 fee.getEligibleFrom(),
                 fee.getFrequency(),
                 fee.getAmount(),
                 fee.getLabel(),
-                fee.getStatus()
-        );
+                fee.getStatus());
     }
 }
